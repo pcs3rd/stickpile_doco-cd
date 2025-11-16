7047504165c390de359c879e4ae33bc14bdaf0ed
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-11-15 21:34:02 -0500

    ENV vars not called

--- shell.nix
+++ shell.nix
@@ -2,4 +2,11 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [ sops age nano lazydocker ];
+
+
+  shellHook = ''
+    alias test="echo 'test'"
+    alias ll="ls -alF"
+  '';
 }
+
