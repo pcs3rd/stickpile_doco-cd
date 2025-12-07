ba1254194948bdc023e6776b9300f5a90bf2733b
pcs3rd <rhean1620@gmail.com>
2025-12-07 11:28:36 -0500

    Update some networking stuff

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@
   shellHook = ''
     alias swarm-deploy=" docker stack deploy doco-cd --compose-file compose.yaml -d && lazydocker"
     alias swarm-undeploy="docker stack rm doco-cd"
-    alias svls="docker service ps $(docker service ls -q) --no-trunc"
+    alias svls="docker network create --driver overlay traefik_backbone && docker service ps $(docker service ls -q) --no-trunc"
     alias rasv="docker service rm $(docker service ls -q)"
   '';
 }
