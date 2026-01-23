66f6cab32230c318a494a1a454d6b121179e220d
pcs3rd <rhean1620@gmail.com>
2026-01-23 16:12:42 -0500

    update data path for tailscale_support stack

--- shell.nix
+++ shell.nix
@@ -6,7 +6,6 @@
 
   shellHook = ''
     alias swarm-deploy=" docker stack deploy doco-cd --compose-file compose.yaml -d && lazydocker"
-    alias kickstart="curl -H \"x-api-key: $(cat /stateful/sys-data/secrets/ws-secret.txt)\" \"http://localhost:1099/v1/api/v1/webhook/$1\""
   '';
 }
 
