86267f9f92f11b20284be66522770e1787d4b98e
pcs3rd <rhean1620@gmail.com>
2026-01-23 12:58:35 -0500

    Kickstart command in nix-shell

--- shell.nix
+++ shell.nix
@@ -1,14 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ sops age nano lazydocker ];
+    nativeBuildInputs = with pkgs.buildPackages; [ sops age nano lazydocker curl ];
 
 
   shellHook = ''
     alias swarm-deploy=" docker stack deploy doco-cd --compose-file compose.yaml -d && lazydocker"
-    alias swarm-undeploy="docker stack rm doco-cd"
-    alias svls="docker service ps $(docker service ls -q) --no-trunc"
-    alias rasv="docker service rm $(docker service ls -q)"
+    alias kickstart="curl -H \"x-api-key: $(cat /stateful/sys-data/secrets/ws-secret.txt)\" \"http://localhost:1099/v1/api/v1/webhook/$1\""
   '';
 }
 
